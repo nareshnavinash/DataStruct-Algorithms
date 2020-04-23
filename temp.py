433325bb4c2b239f12a9d4da4e14e654ff081669
Naresh Sekar <nareshnavinash@gmail.com>
2020-04-23 12:20:08 +0530

    histogram area

--- temp.py
+++ temp.py
@@ -1,38 +1,25 @@
-# Stock Span O(n)
-def find_span(arr):
+# O(n)
+def find_max_area(arr):
     length = len(arr)
-    span = [0 for i in range(length)]
-    span[0] = 1
-    index = [0]
+    i = 0
+    max_val = 0
+    area = 0
+    index_stack = []
 
-    for i in range(1, length):
-        span[i] = 1
-        while len(index) > 0 and arr[index[-1]] <= arr[i]:
-            index.pop()
-        if len(index) > 0:
-            span[i] = i - index[-1]
+    while i < length:
+        if index_stack == [] or arr[index_stack[-1]] <= arr[i]:
+            index_stack.append(i)
+            i += 1
         else:
-            span[i] = i + 1
-        index.append(i)
-    print(span)
+            top_index = index_stack.pop()
+            if not index_stack:
+                area = arr[top_index] * i
+            else:
+                area = arr[top_index] * (i - index_stack[-1] - 1)
+        max_val = max(max_val, area)
+
+    print(max_val)
 
 
-# O(n^2)
-def span(arr):
-    length = len(arr)
-    span = [0 for i in range(length)]
-    span[0] = 1
-
-    for i in range(1, length):
-        span[i] = 1
-        j = i - 1
-        while j >= 0 and arr[i] >= arr[j]:
-            j = j - 1
-            span[i] += 1
-
-    print(span)
-
-
-a = [10, 4, 5, 90, 120, 80]
-find_span(a)
-span(a)
+a = [6, 2, 5, 4, 5, 1, 6]
+find_max_area(a)
