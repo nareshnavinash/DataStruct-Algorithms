48a6afac501b6f0edddfb173dd65f09003445cd4
Naresh Sekar <nareshnavinash@gmail.com>
2020-05-13 19:08:29 +0530

    binary trees

---
+++ Trees/max_min_ele_binary_tree.py
@@ -0,0 +1,58 @@
+from Arrays.Queue import Queue
+
+
+class newNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+
+# Returns maximum value in a
+# given Binary Tree
+def findMax(root):
+    # Base case
+    if (root == None):
+        return float('-inf')
+
+    # Return maximum of 3 values:
+    # 1) Root's data 2) Max in Left Subtree
+    # 3) Max in right subtree
+    res = root.data
+    lres = findMax(root.left)
+    rres = findMax(root.right)
+    if (lres > res):
+        res = lres
+    if (rres > res):
+        res = rres
+    return res
+
+
+def find_min_in_BT(root):
+    if root is None:
+        return float('inf')
+    res = root.data
+    lres = find_min_in_BT(root.left)
+    rres = find_min_in_BT(root.right)
+    if lres < res:
+        res = lres
+    if rres < res:
+        res = rres
+    return res
+
+
+# Driver Code
+if __name__ == '__main__':
+    root = newNode(-2)
+    root.left = newNode(-7)
+    root.right = newNode(-5)
+    root.left.right = newNode(-6)
+    root.left.right.left = newNode(-1)
+    root.left.right.right = newNode(-11)
+    root.right.right = newNode(-9)
+    root.right.right.left = newNode(-4)
+
+    print("Maximum element is", findMax(root))
+    print("Minimum element is", find_min_in_BT(root))
+    q = Queue()
+    print(q.enqueue(root))
+
